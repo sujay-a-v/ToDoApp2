5f4e040bca153dc30051052f9b27c1eabb93e22f
Sujay <sujay.av555@gmail.com>
2017-12-28 19:41:10 +0530

    Thymeleaf

--- src/main/java/com/bridgelabz/controller/NoteController.java
+++ src/main/java/com/bridgelabz/controller/NoteController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bridgelabz.pojo.Notes;
@@ -50,11 +52,11 @@ public class NoteController {
 	}*/
 	
 	@RequestMapping(value="addNote",method = RequestMethod.POST)
-	public ModelAndView addNote(Notes note,HttpServletRequest request, HttpSession session) {
-		/*Notes note = new Notes();
-		Map<String, String[]> map = request.getParameterMap();
-		note.setDescription(map.get("description")[0]);
-		note.setTitle(map.get("title")[0]);*/
+	public ModelAndView addNote(@RequestParam("title") String title, @RequestParam("description") String description, HttpServletRequest request, HttpSession session) {
+		Notes note = new Notes();
+	//	note.setDescription(map.get("description")[0]);
+		note.setTitle(title);
+		note.setDescription(description);
 		User noteUser=(User) session.getAttribute("user");
 		//User noteUser=userService.getByEmail(email);
 		Date date = new Date();
@@ -77,19 +79,14 @@ public class NoteController {
 	@RequestMapping(value="/delete/{id}",method = RequestMethod.DELETE)
 	public ModelAndView deleteNote(@PathVariable int id,HttpSession session) {
 		
-		System.out.println("hdfgb");
-		System.out.println("\n\n note id "+id);
-		
 		Notes currentNote = notesService.fetchById(id);
 		if (currentNote == null) {
 			
 			return null;
 		}
-		System.out.println("not null");
 		notesService.deleteUserNotes(id);
 		User noteUser=(User) session.getAttribute("user");
 		List<Notes> notes=notesService.fetchAllNotes(noteUser);
-		System.out.println("Ajjayya 123");
 		ModelAndView modelAndView=new ModelAndView();
 		Notes note=new Notes();
 		modelAndView.setViewName("home");
@@ -99,6 +96,19 @@ public class NoteController {
 		
 		return modelAndView;
 		
+	}
+	
+	@RequestMapping(value="/edit/{id}",method = RequestMethod.POST)
+	public ModelAndView editNote(@PathVariable int id,HttpSession session) {
+		
+		User noteUser=(User) session.getAttribute("user");
+		Notes currentNote = notesService.fetchById(id);
+		//JSONObject editNote=currentNote;
+		ModelAndView modelAndView=new ModelAndView();
+		modelAndView.setViewName("home");
+		modelAndView.addObject("user1",noteUser);
+		modelAndView.addObject("note",currentNote);
+		return modelAndView;
 	}
 
 }
--- src/main/resources/static/script/noteCrud.js
+++ src/main/resources/static/script/noteCrud.js
@@ -12,7 +12,7 @@ function deleteNote(id) {
     
 }
 
-/*function addNote(){
+function addNote(){
 	var note={};
 	note.title= $('#title').val();
 	note.description = $('#description').val();
@@ -20,10 +20,34 @@ function deleteNote(id) {
 	$.ajax({
 		type:'POST',
 		url:'addNote',
-		data: JSON.stringify(note),
+		data: {
+			'title' : note.title,
+			'description' : note.description
+		},
 	success:function(result){
+		$('#myModal').modal('hide'),
 		console.log(result);
 	}
 	});
-} */
+} 
 
+
+function edit(id) {
+	console.log(id);
+	console.log("ABC");
+	var title=$('#tit').val();
+	var description = $('#des').val();
+	
+	$.ajax({
+   	 type : 'POST',  
+        url : "/edit/"+id,
+        data: {
+			
+		},
+	success:function(result){
+		
+		console.log(result.note);
+	}
+	});
+	
+}
